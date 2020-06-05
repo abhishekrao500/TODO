5d0592337956901665d88b2078e11e496bcc8af7
abhishekrao500 <abhishekrao500@gmail.com>
2020-06-05 20:58:53 +0530

    installed ejs and created viw for home

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,6 @@
 module.exports.home = function(req, res) {
-    return res.end('<h1>Express is up for ToDo</h1>');
+    // return res.end('<h1>Express is up for ToDo</h1>');
+    return res.render('home', {
+        title: "ToDo App (Major Project 1)"
+    });
 }
